8b10be72399c66a51462e7c8cd801b726ea6bab9
Pranab Mehta <prmehta@cisco.com>
2016-01-22 00:32:43 -0500

    More features.

--- install.sh
+++ install.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
 
-if [ ! -d /usr/local/bin ]; then
-    mkdir /usr/local/bin
+BIN_LOCATION="/usr/local/bin"
+ETC_LOCATION="/usr/local/etc"
+
+setArgs () {
+  var=0
+  while [ "$1" != "" ]; do
+    case $1 in
+      "--prefix")
+                          shift
+                          ETC_LOCATION=`cd "$1"; pwd`
+                          BIN_LOCATION=`cd "$1"; pwd`
+                          ;;
+    esac
+    var=$((var+1))
+    shift
+  done
+}
+
+setArgs "$@"
+
+if [ ! -d $BIN_LOCATION ]; then
+    mkdir $BIN_LOCATION
 fi
 
-cp sh/rmed /usr/local/bin/rmed
-chmod a+rx /usr/local/bin/rmed
+echo "Copy rmed to $BIN_LOCATION"
+cp sh/rmed $BIN_LOCATION/rmed
+chmod 755 $BIN_LOCATION/rmed
 
-cp sh/rmed-vimdiff /usr/local/bin/rmed-vimdiff
-chmod a+rx /usr/local/bin/rmed-vimdiff
+echo "Copy rmed-vimdiff to $BIN_LOCATION"
+cp sh/rmed-vimdiff $BIN_LOCATION/rmed-vimdiff
+chmod 755 $BIN_LOCATION/rmed-vimdiff
 
-if [ ! -d /usr/local/etc ]; then
-    mkdir -p /usr/local/etc
+if [ ! -d $ETC_LOCATION ]; then
+    mkdir -p $ETC_LOCATION
 fi
 
-cp colorscheme/rmed.vim /usr/local/etc/rmed.vim
-chmod a+rx /usr/local/etc/rmed.vim
+echo "Copy rmed.vim to $ETC_LOCATION"
+cp colorscheme/rmed.vim $ETC_LOCATION/rmed.vim
+chmod 755 $ETC_LOCATION/rmed.vim
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
-rm /usr/local/bin/rmed
-rm /usr/local/bin/rmed-vimdiff
-rm /usr/local/etc/rmed.vim
+BIN_LOCATION="/usr/local/bin"
+ETC_LOCATION="/usr/local/etc"
+
+setArgs () {
+  var=0
+  while [ "$1" != "" ]; do
+    case $1 in
+      "--prefix")
+                          shift
+                          ETC_LOCATION=`cd "$1"; pwd`
+                          BIN_LOCATION=`cd "$1"; pwd`
+                          ;;
+    esac
+    var=$((var+1))
+    shift
+  done
+}
+
+echo "Removing $BIN_LOCATION/rmed"
+rm $BIN_LOCATION/rmed
+
+echo "Removing $BIN_LOCATION/rmed-vimdiff"
+rm $BIN_LOCATION/rmed-vimdiff
+
+echo "Removing $ETC_LOCATION/rmed.vim"
+rm $ETC_LOCATION/rmed.vim
